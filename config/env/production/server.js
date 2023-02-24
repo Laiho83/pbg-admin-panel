1e3acde58439ec7397cb29ac9f4b09f10d788e20
Laiho83 <samir.kabir83@gmail.com>
2023-02-24 11:01:16 +0100

    rever server prod

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,10 +1,5 @@
 // Path: ./config/env/production/server.js`
 
 module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  app: {
-    keys: env.array("APP_KEYS"),
-  },
-  port: env.int("PORT"),
+  url: env("MY_HEROKU_URL"),
 });
