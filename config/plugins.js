4e942fc0484dfc2bb7cc51f5b8646d8960a49a72
Laiho83 <samir.kabir83@gmail.com>
2023-05-17 18:45:59 +0200

    Remove Sendgrid api key

--- config/plugins.js
+++ config/plugins.js
@@ -5,8 +5,7 @@ module.exports = ({ env }) => ({
     config: {
       provider: "sendgrid",
       providerOptions: {
-        apiKey:
-          "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",
+        apiKey: SENDGRID_API_KEY,
       },
       settings: {
         defaultFrom: "samir.kabir83@gmail.com",
