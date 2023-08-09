b4b5cff9950d053c43e99e8437220dbb7859c173
Laiho83 <samir.kabir83@gmail.com>
2023-08-09 21:07:42 +0200

    Reverting strapi configs back for heroku

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,14 +1,14 @@
 const parse = require("pg-connection-string").parse;
-const config = parse(process.env);
+const config = parse(process.env.DATABASE_URL);
 module.exports = ({ env }) => ({
   connection: {
     client: "postgres",
     connection: {
-      host: env.DATABASE_HOST,
-      port: env.DATABASE_PORT,
-      database: env.DATABASE,
-      user: env.DATABASE_USER,
-      password: env.DATABASE_PASSWORD,
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
         rejectUnauthorized: false,
       },
--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,5 @@
 // Path: ./config/env/production/server.js`
 
 module.exports = ({ env }) => ({
-  url: env("AWS_URL"),
+  url: env("MY_HEROKU_URL"),
 });
